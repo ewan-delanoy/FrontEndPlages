a01be4de760e6d7ac91ad51a572e25208fe8cf43
Ewan Delanoy <ewan.delanoy@zoho.com>
2024-02-13 06:42:56 +0100

    replace scss with css in app and signin

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss'],
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent implements OnInit {
   signinForm: FormGroup;
